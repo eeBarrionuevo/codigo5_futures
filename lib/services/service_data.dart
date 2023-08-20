b18990a73446d0c0c647f40419e4ae28f5ac3652
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-08-20 13:01:03 -0500

    Mods

---
+++ lib/services/service_data.dart
@@ -0,0 +1,18 @@
+class ServiceData {
+  Future<Map> getProfile() async {
+    return Future.delayed(const Duration(seconds: 3), () {
+      return {
+        "id": 1,
+        "fullName": "Juan Manuel",
+        "role": "Desarrollador",
+        "social": {
+          "articles": 43,
+          "following": 234,
+          "rating": 6.3,
+        },
+        "information":
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500",
+      };
+    });
+  }
+}
